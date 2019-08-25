df63e7b533117b38082d692fdddd521fbe5780e8
mr.scrpt <mr.scrpt@gmail.com>
2019-08-25 12:37:25 +0300

    saga change - delet while

--- src/modules/RoverPhotos/sagas.js
+++ src/modules/RoverPhotos/sagas.js
@@ -13,34 +13,16 @@ export function* fetchDataFlow(action) {
   const rovers = yield select(getRovers);
   const apiKey = yield select(getIsAuthorized);
   const res = {};
-  while (true){
-    const sol = action.payload;
 
-    for(let rover of rovers){
-      const roverPhoto = yield call(getPhotos, apiKey, rover, sol);
-      res[rover] = {[sol] : {isLoading: false, ...roverPhoto, isLoaded: true}}
+  const sol = action.payload;
 
-    }
-    yield put(fetchPhotosSuccess(res));
+  for(let rover of rovers){
+    const roverPhoto = yield call(getPhotos, apiKey, rover, sol);
+    res[rover] = {[sol] : {isLoading: false, ...roverPhoto, isLoaded: true}}
   }
 
+  yield put(fetchPhotosSuccess(res));
 
- /* const sol = action.payload;
-  try{
-    const apiKey = yield select(getIsAuthorized);
-    const curiosityPhoto = yield call(getPhotos, apiKey, 'curiosity', sol);
-    const opportunityPhoto = yield call(getPhotos, apiKey, 'opportunity', sol);
-    const spiritPhoto = yield call(getPhotos, apiKey, 'spirit', sol);
-
-    const res = {
-      curiosity: {[sol] : {isLoading: false, ...curiosityPhoto, isLoaded: true}},
-      opportunity: {[sol] :{isLoading: false, ...opportunityPhoto, isLoaded: true}},
-      spirit: {[sol] :{isLoading: false, ...spiritPhoto, isLoaded: true}}
-    };
-    yield put(fetchPhotosSuccess(res));
-  }catch (e) {
-
-  }*/
 }
 
 export default function*() {
